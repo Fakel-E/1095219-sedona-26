93df62c9590a6a37b914ced082e641d0230995a9
Evgeniy Semenov <SemenovEvg1990@gmail.com>
2019-10-28 21:47:39 +0300

    Валидатор-ок. CSS - только минифцировать.

--- js/script.js
+++ js/script.js
@@ -1,20 +1,15 @@
 var btn = document.querySelector(".search-main");
 var popup = document.querySelector(".popup");
 var date = popup.querySelector("[name=check-in]");
-var btn = document.querySelector(".search-main");
 var form = document.querySelector("form");
 
 btn.addEventListener("click", function(evt) {
   evt.preventDefault();
-  popup.classList.add("visually-hidden");
-  popup.classList.remove("popup-animation");
+  popup.classList.toggle("visually-hidden");
+  popup.classList.toggle("popup-animation");
   date.focus();
 })
 
-btn.addEventListener("dblclick", function(evt) {
-  evt.preventDefault();
-  popup.classList.remove("visually-hidden");
-})
 window.addEventListener("keydown", function(evt) { /*закрываем попап по esc*/
   if (evt.keyCode === 27) {
     if (popup.classList.contains("popup")) {
@@ -23,17 +18,14 @@ window.addEventListener("keydown", function(evt) { /*закрываем попа
         }
     }
 })
-btn.addEventListener("click", function(evt){
-  if (popup.classList.contains("popup")){
-    evt.preventDefault();
-    popup.classList.add("popup-animation");
-    }
-})
 
 form.addEventListener("submit", function (evt) {
   if (!date.value)
   evt.preventDefault();
   popup.classList.add("popup-err");
+  function del(){
+    popup.classList.remove("popup-err");
+  }
+  setTimeout(del, 800);
+  
 })
-
-/*Вопрос: Как сделать, что окно скрывалось на второй клик? Не могу понять, как использовать classToogle*/
